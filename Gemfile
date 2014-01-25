98a35b6fbccc0688148699fb5fbe8fd13ff27b64
Sergey Zonov <calipsod123@gmail.com>
2014-01-25 15:32:52 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'rails', '4.0.2'
+
+
+gem 'pg', '0.15.1'
+gem 'rails_12factor', '0.0.2'
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails', '3.0.4'
+gem 'turbolinks', '1.1.1'
+gem 'jbuilder', '~> 1.2'
+gem 'haml'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Untitled::Application.config.secret_key_base = 'acc419e92d8de75c91e9d15b853825222ec01d4c6f40810047b40cf336f27080a6f54673b3bd4253951448109015026f9bf7bad9def81a57dc90b7c10b08ba1f'
